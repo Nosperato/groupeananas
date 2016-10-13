60670a5b6e0ae6f54402b93e82f640754d6f274d
Nosperato <yoan.bragado@hotmail.fr>
2016-10-13 18:00:35 +0200

    Add files via upload

---
+++ monappli/src/main/java/com/objis/mavenapps/App.java
@@ -0,0 +1,14 @@
+package com.objis.mavenapps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("oucou");
+    }
+}
