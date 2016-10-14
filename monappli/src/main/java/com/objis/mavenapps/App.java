317cc941612df56b71d7808008a7d2224e217b01
Nosperao <yoan.bragado@hotmail.fr>
2016-10-14 14:48:54 +0200

    Modification d'une ligne

--- monappli/src/main/java/com/objis/mavenapps/App.java
+++ monappli/src/main/java/com/objis/mavenapps/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "OUAIsalut" );
+        System.out.println( "OUAI" );
     }
 }
