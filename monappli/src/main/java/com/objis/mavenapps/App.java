f0ec9d09681c327f94bd5f55166d1f07bf009d3f
Tetrisman42 <joshuatorri@hotmail.fr>
2016-10-14 10:21:23 +0200

    Add files via upload

--- monappli/src/main/java/com/objis/mavenapps/App.java
+++ monappli/src/main/java/com/objis/mavenapps/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "He t'aime" );
+        System.out.println( "Moi je t'aime pas" );
         
     }
 }
